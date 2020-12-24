c2a28df58e1babd6afbcea97fef86e41a14c517f
peterpan <apppeterpan@gmail.com>
2020-12-24 23:56:20 +0800

    Initial Commit

---
+++ KBK2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KBK2
+//
+//  Created by SHIH-YING PAN on 2020/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
